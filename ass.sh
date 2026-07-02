e30f366b742f4090927e52c5204125ae766e68eb
zeretrelle <zeretrelle@gmail.com>
2026-07-02 15:12:52 +0300

    task#1

--- ass.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo "Branch: shell-script. Attempt #2"
-cat > ass.sh <<EOF
-#!/bin/sh
-echo "Branch: shell-script. Attempt #2"
-
--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-echo "Branch: shell-script"
+echo "Branch: shell-script. version 2"
